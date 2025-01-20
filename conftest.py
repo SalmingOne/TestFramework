ea8d5f997812b6a8d4f7ca580d5ff3e5e0431ef5
SalmingOne <corp.wnsmir@gmail.com>
2025-01-20 22:58:30 +0300

    Настроен Dockerfile и docker-compose.yml

--- conftest.py
+++ conftest.py
@@ -2,14 +2,26 @@ import os
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from data.urls import Urls
 from pages.base_page import BasePage
 
+CI = os.getenv('CI')
+
 @pytest.fixture(scope="class")
 def driver():
-    driver = webdriver.Chrome(options=webdriver.ChromeOptions(), service=ChromeService())
-    driver.maximize_window()
+    if CI == 'false':
+        driver = webdriver.Chrome(service=Service(), options=Options())
+        driver.maximize_window()
+    else:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        service = Service("/usr/bin/chromedriver")
+        driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
 
